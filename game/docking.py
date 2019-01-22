bb5ac21e34bef7d6951dfcd8180e0e8bf98365e2
nioky <ndx.nicolas.deschaux@gmail.com>
2019-01-22 17:00:16 +0100

    handling docking and reinititalizing logics

--- game/docking.py
+++ game/docking.py
@@ -14,47 +14,66 @@ class DockingGame(arcade.Window):
                  resizable: bool = False):
         super().__init__(width, height, title, fullscreen, resizable)
         arcade.set_background_color(arcade.color.AMAZON)
-        self.ats = None
+        self.atv = None
         self.visor = None
+        self.debug = True
 
     def setup(self):
         # Set up your game here
-        self.ats = ScalingSprite(self, "./game/images/ats.png", SPRITE_SCALING)
+        self.atv = ScalingSprite(self, "./game/images/ats.png", scale=SPRITE_SCALING)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
-        self.ats.draw()
+        self.atv.draw()
         self.visor.draw()
+        self.drawDebug()
+        self.drawState()
+
+    def drawDebug(self):
+        if self.debug:
+            scale, position, velocity, initialising, dock = self.atv.getProperties()
+            arcade.draw_text(str(position), 50, 50, arcade.color.WHITE, 12)
+            arcade.draw_text(str(initialising), 50, 35, arcade.color.WHITE, 12)
+            arcade.draw_text(str(dock), 50, 20, arcade.color.WHITE, 12)
+
+    def drawState(self):
+        _, _, _, initialising, dock = self.atv.getProperties()
+        if initialising:
+            arcade.draw_text("INITIALIZING", self.width/2, self.height / 2,
+                             arcade.color.RED, 100, width=200, align='center',
+                             anchor_x='center', anchor_y='center', rotation=0)
+        if dock:
+            arcade.draw_text("DOCKING COMPLETE", self.width/2, self.height / 2,
+                             arcade.color.GREEN, 100, width=200, align='center',
+                             anchor_x='center', anchor_y='center', rotation=0)
 
     def on_key_press(self, key, modifiers):
         xy_delta_speed = 0.05
         z_delta_speed = 0.00005
-        v_x = self.ats.change_x
-        v_y = self.ats.change_y
 
         if key == arcade.key.UP:
-            self.ats.change_y = self.ats.change_y + xy_delta_speed
+            self.atv.change_y = self.atv.change_y + xy_delta_speed
         elif key == arcade.key.DOWN:
-            self.ats.change_y = self.ats.change_y - xy_delta_speed
+            self.atv.change_y = self.atv.change_y - xy_delta_speed
         elif key == arcade.key.LEFT:
-            self.ats.change_x = self.ats.change_x - xy_delta_speed
+            self.atv.change_x = self.atv.change_x - xy_delta_speed
         elif key == arcade.key.RIGHT:
-            self.ats.change_x = self.ats.change_x + xy_delta_speed
+            self.atv.change_x = self.atv.change_x + xy_delta_speed
         elif key == arcade.key.A:
-            self.ats.change_z = self.ats.change_z + z_delta_speed
+            self.atv.change_z = self.atv.change_z + z_delta_speed
         elif key == arcade.key.Q:
-            self.ats.change_z = self.ats.change_z - z_delta_speed
+            self.atv.change_z = self.atv.change_z - z_delta_speed
         elif key == arcade.key.F:
             # User hits f. Flip between full and not full screen.
             self.set_fullscreen(not self.fullscreen)
 
             # Get the window coordinates. Match viewport to window coordinates
             # so there is a one-to-one mapping.
-            width, height = self.get_size()
-            self.set_viewport(0, width, 0, height)
+            # width, height = self.get_size()
+            # self.set_viewport(0, width, 0, height)
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
-        self.ats.update()
+        self.atv.update()
         self.visor = Visor(self.width, self.height)
--- game/scaling_sprite.py
+++ game/scaling_sprite.py
@@ -1,9 +1,12 @@
 from arcade.sprite import Sprite
 
-DOCKING_POS_X = 0
-DOCKING_POS_Y = 0
-DOCKING_XY_TOL = 10
-DOCKING_SCALE = 10
+ATV_IMG_WIDTH = 8822
+ATV_IMG_HEIGHT = 5881
+TARGET_IMG_POS_X = 4832
+TARGET_IMG_POS_Y = 2391
+XY_TOL = 10
+SCALE_TOL = 0.01
+DOCKING_SCALE = 1
 LIMIT_POS_X = 0
 LIMIT_POS_Y = 0
 INIT_DELTA_TIME = 200
@@ -19,8 +22,10 @@ class ScalingSprite(Sprite):
     def __init__(self, arcade_window, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0,
                  image_width: float = 0, image_height: float = 0, repeat_count_x=1, repeat_count_y=1):
         self.arcade_window = arcade_window
-        self.init_pos_x = self.arcade_window.width / 2
-        self.init_pos_y = self.arcade_window.height / 2
+        self.init_pos_x = self.arcade_window.width * 0.5
+        self.init_pos_y = self.arcade_window.height * 0.5
+        self.docking_pos_x = self.arcade_window.width / 2
+        self.docking_pos_y = self.arcade_window.height / 2
         self.init_scale = scale
         super().__init__(filename, scale, image_x, image_y, image_width, image_height, self.init_pos_x, self.init_pos_y,
                          repeat_count_x, repeat_count_y)
@@ -58,9 +63,13 @@ class ScalingSprite(Sprite):
 
     def checkPosition(self):
         if self.isInitialising:
-            pass
-        if ((self.center_x < DOCKING_POS_X + DOCKING_XY_TOL) and (self.center_x > DOCKING_POS_X - DOCKING_XY_TOL)
-                and (self.center_y < DOCKING_POS_Y + DOCKING_XY_TOL) and (self.center_y > DOCKING_POS_Y - DOCKING_XY_TOL)
+            if ((self.center_x < self.init_pos_x + XY_TOL) and (self.center_x > self.init_pos_x - XY_TOL)
+                    and (self.center_y < self.init_pos_y + XY_TOL) and (self.center_y > self.init_pos_y - XY_TOL)
+                    and (self.scale < self.init_scale + SCALE_TOL)):
+                self.isInitialising = False
+                self.stop()
+        if ((self.center_x < self.docking_pos_x + XY_TOL) and (self.center_x > self.docking_pos_x - XY_TOL)
+                and (self.center_y < self.docking_pos_y + XY_TOL) and (self.center_y > self.docking_pos_y - XY_TOL)
                 and (self.scale > DOCKING_SCALE)):
             self.isDock = True
         elif (self.center_x < 0) or (self.center_x > self.width) \
@@ -68,10 +77,16 @@ class ScalingSprite(Sprite):
                 or (self.scale > DOCKING_SCALE):
             self.reinitialisation()
         if self.isDock:
-            self.change_x, self.change_y, self.change_z = 0, 0, 0
+            self.stop()
+
+    def stop(self):
+        self.change_x, self.change_y, self.change_z = 0, 0, 0
 
     def reinitialisation(self):
         self.isInitialising = True
         self.change_x = (self.init_pos_x - self.center_x) / INIT_DELTA_TIME
         self.change_y = (self.init_pos_y - self.center_y) / INIT_DELTA_TIME
         self.change_z = (self.init_scale - self.scale) / INIT_DELTA_TIME
+
+    def getProperties(self):
+        return self.scale, self.position, self.velocity, self.isInitialising, self.isDock
