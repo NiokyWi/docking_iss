e7f0fc0e6255f353485262b0aaf1e3093a2b86ca
nioky <ndx.nicolas.deschaux@gmail.com>
2019-04-17 13:44:29 +0200

    Remove todolist

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import arcade
 from game.game import Game
 
-'''
-TO DO list:
-- push key -> jet (not only one)
-- add perturbation in order to make sure the ATS doesn't go straight
-
-BONUS:
-- set difficulties levels
-'''
-
 FULLSCREEN = True
 DEBUG_MODE = False
 SCALE_SPEED = 0.001
